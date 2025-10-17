f2cebc7cfe92009326de8e50dcb5ec722264783e
indrareddy12 <indrasenareddybathini86@gmail.com>
2025-10-17 16:02:44 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Indra Sena reddy ',
+    author_email= 'indrasenareddybathini83@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
